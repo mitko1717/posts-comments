40642bbdf35f7efe6dfd94285a15b8c3c96a6a36
Dmytro Hirich <diman12345677@gmail.com>
2023-03-21 16:45:01 +0200

    fix

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -21,7 +21,7 @@ const Post = ({ id }: IPostProps) => {
 
   if (data)
     return (
-      <div className="h-[100vh] overflow-y-auto pb-8">
+      <div className="h-[100vh] w-[80%] overflow-y-auto pb-8 mx-auto">
         <Link to={`/`}>
           <Button variant="contained">back to posts list</Button>
         </Link>
@@ -42,7 +42,7 @@ const Post = ({ id }: IPostProps) => {
           ) : (
             <h4 className="my-4 font-bold ml-8">no comments for this post</h4>
           )}
-          {data.comments?.map((comment) => (
+          {[...data.comments]?.reverse().map((comment) => (
             <Comment key={comment.id} comment={comment} />
           ))}
         </div>
